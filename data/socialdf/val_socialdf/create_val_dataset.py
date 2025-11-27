9014dceba7b2819c67b035bc9ce3f772556577b7
CaioRhoden <caiorhoden@gmail.com>
2025-11-26 23:18:12 -0300

    createing val dataset for

--- data/socialdf/val_socialdf/create_val_dataset.py
+++ data/socialdf/val_socialdf/create_val_dataset.py
@@ -2,4 +2,12 @@ import polars as pl
 
 def create_val_dataset():
     
-    df = pl.read_csv("")
+    df = pl.read_csv("../socialdf_vids/socialdf_processed.csv")
+    is_real = df.filter(pl.col("target") == 0).sample(n=50, seed=42)
+    is_fake = df.filter(pl.col("target") == 1).sample(n=50, seed=42)
+
+    val_df = pl.concat([is_real, is_fake])
+    val_df.write_csv("val_socialdf.csv")
+
+if __name__ == "__main__":
+    create_val_dataset()
